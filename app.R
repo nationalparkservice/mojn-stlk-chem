feb780d01f123c27c57d9d3b6a35305940ca6af8
Sarah Wright <s.wright.618@gmail.com>
2018-05-30 13:35:13 -0700

    Added code to connect to database and fetch water chem data into a dataframe

--- app.R
+++ app.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(plotly)
+library(RODBC)
+library(tidyverse)
 
 # Define UI
 ui <- fluidPage(
@@ -23,14 +25,20 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
   # Connect to the database
-  
+  LAKES.dsn <- 'driver={SQL Server Native Client 11.0};server=INPLAKE52V\\MOJN;database=MOJN_Lakes;trusted_connection=Yes;applicationintent=readonly'
+  LAKES.db <- odbcDriverConnect(LAKES.dsn)
   # Pull water chem data into a dataframe
+  chem <- sqlFetch(LAKES.db, "analysis.WaterChemistry")
+  # Close database connection
+  close(LAKES.db)
   
   # Generate water chem parameter select input
+  chem.params <- levels(chem$CharacteristicLabel)
   
   # Get the water chem parameter from the dropdown
   
   # Generate one output plot per lake
+  lakes <- levels(chem$Site)
   
 }
 
